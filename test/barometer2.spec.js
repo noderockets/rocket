f85d58f18e96e1572f07252f06b82f9d0cac2fc1
Thomas A. Valletta <tom@vallettas.com>
2018-07-12 04:09:32 +0000

    in work

--- test/barometer2.spec.js
+++ test/barometer2.spec.js
@@ -11,30 +11,30 @@ const bmp280 = new BMP280(OPTIONS);
 const SEA_LEVEL_PRESSURE = 101325
 const ALTITUDE_PRESSURE_CONSTANTS = {
   n1: 44330,
-  n2: 0.1903
+  n2: 0.19022256039566293
 }
 
 bmp280.config({
-  powerMode: 1,               // 0 - sleep, 1|2 - one measurement, 3 - continuous
-  pressureOversampling: 3,    // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
-  temperatureOversampling: 1, // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
+  powerMode: 3,               // 0 - sleep, 1|2 - one measurement, 3 - continuous
+  pressureOversampling: 4,    // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
+  temperatureOversampling: 2, // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
   iirFilter: 2,               // Coefficient: 0 - off, 1 - 2, 2 - 4, 3 - 8, 4 - 16
-  standby: 4                  // 0 - 0.5ms, 1 - 62.5ms, 2 - 125ms, 3 - 250ms, 4 - 500ms, 5 - 1000ms, 6 - 2000ms, 7 - 4000ms
+  standby: 1                  // 0 - 0.5ms, 1 - 62.5ms, 2 - 125ms, 3 - 250ms, 4 - 500ms, 5 - 1000ms, 6 - 2000ms, 7 - 4000ms
 })
 
 console.log(`Reading sensors`)
 setInterval(async () => {
   const { Temperature, Pressure, Altitude } = await getValues()
-  console.log(`Temperture:\t${Temperature}`)
+  console.log(`Temperature:\t${Temperature}`)
   console.log(`Pressure:\t${Pressure}`)
   console.log(`Altitude:\t${Altitude}`)
-}, 1000)
+}, 100)
 
 function getValues() {
   return bmp280
     .readSensors()
     .then(data => {
-      const Altitude = getAltitude(data.Pressure)
+      const Altitude = getAltitude(data.Pressure, data.Temperature)
       return { ...data, Altitude }
     })
     .catch(err => {
@@ -48,7 +48,9 @@ process.on("SIGINT", () => {
   process.exit()
 })
 
-function getAltitude (pressure) {
+function getAltitude(pressure, tempurature) {
   const { n1, n2 } = ALTITUDE_PRESSURE_CONSTANTS
-  return n1 * (1.0 - Math.pow(pressure / SEA_LEVEL_PRESSURE, n2))
+  const SEA_LEVEL_PRESSURE = 1013.25
+  return ((Math.pow((SEA_LEVEL_PRESSURE / pressure), n2) - 1.0) * (tempurature + 273.15)) / 0.0065
 }
+
