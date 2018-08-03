c45d219908b403c8607e5be513db53c54902323c
Thomas A. Valletta <tom@vallettas.com>
2018-08-03 06:36:57 -0600

    adjust barometer default configuration

--- driver/bmp280.js
+++ driver/bmp280.js
@@ -15,11 +15,11 @@ const ALTITUDE_PRESSURE_CONSTANTS = {
 }
 
 bmp280.config({
-  powerMode: 1,               // 0 - sleep, 1|2 - one measurement, 3 - continuous
-  pressureOversampling: 3,    // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
-  temperatureOversampling: 1, // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
+  powerMode: 3,               // 0 - sleep, 1|2 - one measurement, 3 - continuous
+  pressureOversampling: 4,    // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
+  temperatureOversampling: 2, // 0 - Skipped, 1 - ×1, 2 - ×2, 3 - ×4, 4 - ×8, 5 - ×16
   iirFilter: 2,               // Coefficient: 0 - off, 1 - 2, 2 - 4, 3 - 8, 4 - 16
-  standby: 4                  // 0 - 0.5ms, 1 - 62.5ms, 2 - 125ms, 3 - 250ms, 4 - 500ms, 5 - 1000ms, 6 - 2000ms, 7 - 4000ms
+  standby: 1                  // 0 - 0.5ms, 1 - 62.5ms, 2 - 125ms, 3 - 250ms, 4 - 500ms, 5 - 1000ms, 6 - 2000ms, 7 - 4000ms
 })
 
 let zero = 0
