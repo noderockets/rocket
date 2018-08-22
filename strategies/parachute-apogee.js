558cded5d2e2710acb54b466243e656ba99daac8
Almir Bijedic <almir.bij@gmail.com>
2018-08-22 16:12:31 -0700

    Fix bug with lastAltitude missing

--- strategies/parachute-apogee.js
+++ strategies/parachute-apogee.js
@@ -1,46 +1,47 @@
 class ParachuteApogee {
-  rocketDidActivate () {
-    this.log('Using apogee detection based strategy')
-    this.parachuteArmed = true
-    this.reset()
+  rocketDidActivate() {
+    this.log("Using apogee detection based strategy");
+    this.parachuteArmed = true;
+    this.reset();
   }
 
   reset() {
-    this.lastAltitude = Number.MIN_VALUE
-    this.descentCount = 0
+    this.lastAltitude = Number.MIN_VALUE;
+    this.descentCount = 0;
   }
 
-  parachuteDidArm () {
-    this.parachuteArmed = true
+  parachuteDidArm() {
+    this.parachuteArmed = true;
   }
 
-  parachuteDidDisarm ()  {
-    this.parachuteArmed = false
+  parachuteDidDisarm() {
+    this.parachuteArmed = false;
   }
 
-  rocketDidEmitData (data) {
-    if (!this.parachuteArmed) return
-    var currentAltitude = data.altitude
+  rocketDidEmitData(data) {
+    if (!this.parachuteArmed) return;
+    var currentAltitude = data.altitude;
 
-    if (lastAltitude < data.altitude) {
-      this.descentCount = 0
-      return
+    if (this.lastAltitude < data.altitude) {
+      this.descentCount = 0;
+      return;
     }
 
     if (++this.descentCount > this.props.threshold) {
-      this.emit('deploy-parachute')
-      this.emit('disarm-parachute')
-      this.reset()
+      this.emit("deploy-parachute");
+      this.emit("disarm-parachute");
+      this.reset();
     }
   }
 }
-ParachuteApogee.displayName = 'Parachute: Apogee'
-ParachuteApogee.description = 'Waits for the rocket to start moving downward, then deploys the parachute.'
+ParachuteApogee.displayName = "Parachute: Apogee";
+ParachuteApogee.description =
+  "Waits for the rocket to start moving downward, then deploys the parachute.";
 ParachuteApogee.propTypes = {
   threshold: {
-    description: 'Number of measurements to verify rocket is falling.',
-    type: 'number',
+    description: "Number of measurements to verify rocket is falling.",
+    type: "number",
     default: 5
   }
-}
-module.exports = ParachuteApogee
+};
+module.exports = ParachuteApogee;
